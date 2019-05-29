fd2aff1b0ff7a0c0acbbce454cc3187309eaab90
Chacha Sikes <chachasikes@gmail.com>
2019-05-29 11:11:11 -0700

    move touch controllers

--- public/js/AframeFromJson.js
+++ public/js/AframeFromJson.js
@@ -273,7 +273,7 @@ export class AframeFromJson {
                       <a-camera ${className} id="${props.id}">
                       ${fadeMask}
                       ${touchContollers}
-                      orientationOffset="0.0001 0.0001 0.0001"
+                      orientationOffset="-0.01 -0.01 -0.01"
                       </a-camera>
                     </a-entity>`;
                 }
