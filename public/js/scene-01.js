564ea06ee73b0244404d83515ea2c806cc707422
Chacha Sikes <chachasikes@gmail.com>
2019-05-29 11:08:23 -0700

    move touch controllers

--- public/js/scene-01.js
+++ public/js/scene-01.js
@@ -20,9 +20,9 @@ export let Scene = {
           glb: 'https://dl.dropboxusercontent.com/s/u9o72m7y4mocn4s/dried_yellow_rose.glb',
           id: 'left-rose-hand',
           scale: {
-            x: 0.001,
-            y: 0.001,
-            z: 0.001
+            x: 0.005,
+            y: 0.005,
+            z: 0.005
           },
         },
       },
