1cc2a9fef7a8f12c8ba33e692cddf6746ddc04e1
Chacha Sikes <chachasikes@gmail.com>
2019-06-02 11:06:07 -0700

    x button testing

--- public/js/StoryboxAframe.js
+++ public/js/StoryboxAframe.js
@@ -19,17 +19,13 @@ export class StoryboxAframe {
   }
 
   getProperties(label, data) {
-    if (
-      data !== undefined &&
-      data[label] !== undefined
-    ) {
-
+    if (data !== undefined && data[label] !== undefined) {
       let props = data[label];
       let options = ``;
       Object.keys(props).map(prop => {
-        options = `${options}${prop}=${props[prop]};`
+        options = `${options}${prop}=${props[prop]};`;
       });
-    return `${label}=${options}`;
+      return `${label}=${options}`;
     }
     return ``;
   }
@@ -63,8 +59,12 @@ export class StoryboxAframe {
       data[label].depth !== undefined
     ) {
       return {
-        tag: `width="${data[label].width}" height="${data[label].height}" depth="${data[label].depth}"`,
-        attributes: `${data[label].width} ${data[label].height} ${data[label].depth}`
+        tag: `width="${data[label].width}" height="${
+          data[label].height
+        }" depth="${data[label].depth}"`,
+        attributes: `${data[label].width} ${data[label].height} ${
+          data[label].depth
+        }`
       };
     }
     return ``;
@@ -73,41 +73,63 @@ export class StoryboxAframe {
   getCube(data) {
     let cube = [
       {
-        position: `position="${data.position.x} ${data.position.y} ${data.position.z - (data.scale.z / 2)}"`,
-        rotation: `rotation="${data.rotation.x} ${data.rotation.y} ${data.rotation.z}"`,
-        art: typeof data.art === "object" ? data.art.back : data.art,
+        position: `position="${data.position.x} ${data.position.y} ${data
+          .position.z -
+          data.scale.z / 2}"`,
+        rotation: `rotation="${data.rotation.x} ${data.rotation.y} ${
+          data.rotation.z
+        }"`,
+        art: typeof data.art === "object" ? data.art.back : data.art
       },
       {
-        position: `position="${data.position.x} ${data.position.y} ${data.position.z + (data.scale.z / 2)}"`,
-        rotation: `rotation="${data.rotation.x} ${data.rotation.y} ${data.rotation.z}"`,
-        art: typeof data.art === "object" ? data.art.front : data.art,
+        position: `position="${data.position.x} ${data.position.y} ${data
+          .position.z +
+          data.scale.z / 2}"`,
+        rotation: `rotation="${data.rotation.x} ${data.rotation.y} ${
+          data.rotation.z
+        }"`,
+        art: typeof data.art === "object" ? data.art.front : data.art
       },
       {
-        position: `position="${data.position.x} ${data.position.y - (data.scale.y / 2)} ${data.position.z}"`,
-        rotation: `rotation="${data.rotation.x - 90} ${data.rotation.y} ${data.rotation.z}"`,
-        art: typeof data.art === "object" ? data.art.bottom : data.art,
+        position: `position="${data.position.x} ${data.position.y -
+          data.scale.y / 2} ${data.position.z}"`,
+        rotation: `rotation="${data.rotation.x - 90} ${data.rotation.y} ${
+          data.rotation.z
+        }"`,
+        art: typeof data.art === "object" ? data.art.bottom : data.art
       },
       {
-        position: `position="${data.position.x} ${data.position.y + (data.scale.y / 2)} ${data.position.z}"`,
-        rotation: `rotation="${data.rotation.x + 90} ${data.rotation.y} ${data.rotation.z}"`,
-        art: typeof data.art === "object" ? data.art.top : data.art,
+        position: `position="${data.position.x} ${data.position.y +
+          data.scale.y / 2} ${data.position.z}"`,
+        rotation: `rotation="${data.rotation.x + 90} ${data.rotation.y} ${
+          data.rotation.z
+        }"`,
+        art: typeof data.art === "object" ? data.art.top : data.art
       },
       {
-        position: `position="${data.position.x  - (data.scale.x / 2)} ${data.position.y} ${data.position.z}"`,
-        rotation: `rotation="${data.rotation.x} ${data.rotation.y - 90} ${data.rotation.z}"`,
-        art: typeof data.art === "object" ? data.art.left : data.art,
+        position: `position="${data.position.x - data.scale.x / 2} ${
+          data.position.y
+        } ${data.position.z}"`,
+        rotation: `rotation="${data.rotation.x} ${data.rotation.y - 90} ${
+          data.rotation.z
+        }"`,
+        art: typeof data.art === "object" ? data.art.left : data.art
       },
       {
-        position: `position="${data.position.x + (data.scale.x / 2)} ${data.position.y} ${data.position.z}"`,
-        rotation: `rotation="${data.rotation.x} ${data.rotation.y + 90} ${data.rotation.z}"`,
-        art: typeof data.art === "object" ? data.art.right : data.art,
-      },
+        position: `position="${data.position.x + data.scale.x / 2} ${
+          data.position.y
+        } ${data.position.z}"`,
+        rotation: `rotation="${data.rotation.x} ${data.rotation.y + 90} ${
+          data.rotation.z
+        }"`,
+        art: typeof data.art === "object" ? data.art.right : data.art
+      }
     ];
     return cube;
   }
 
   playGLTFAnimation(e) {
-    console.log(e, 'playing gltf animation');
+    console.log(e, "playing gltf animation");
   }
 
   // Format json object as a-frame
@@ -119,7 +141,6 @@ export class StoryboxAframe {
       Object.keys(json).map(item => {
         if (Object.keys(json[item]).length > 0) {
           Object.keys(json[item]).map(propKey => {
-
             // Set up variables
             let props = json[item][propKey];
             let scale = this.getAxis("scale", props);
@@ -131,23 +152,31 @@ export class StoryboxAframe {
             let dimensions = this.getDimensions("dimensions", props);
             let classProps = this.getValue("className", props);
             let className = `class="${classProps.attribute}"`;
-            let line = this.getProperties('line', props);
+            let line = this.getProperties("line", props);
             switch (propKey) {
               case "sky":
                 if (props.color !== undefined) {
-                  innerMarkup = `${innerMarkup}<a-sky color="${color.attribute}"></a-sky>`;
+                  innerMarkup = `${innerMarkup}<a-sky color="${
+                    color.attribute
+                  }"></a-sky>`;
                 } else if (
                   props.color === undefined &&
                   props.art !== undefined &&
                   props.id !== undefined
                 ) {
                   assetsElements.push(
-                    `<img ${className} id="${props.id}" src="${props.art}" crossorigin="anonymous" preload="true" />`
+                    `<img ${className} id="${props.id}" src="${
+                      props.art
+                    }" crossorigin="anonymous" preload="true" />`
                   );
 
                   // HACK force a quick rotation to easily fix any weirdly oriented skybox art.
                   innerMarkup = `${innerMarkup}
-                    <a-sky ${className} src="#${props.id}" ${color.attribute} animation="property: rotation; to: ${rotation.attributes}; dur: 10" preload="auto"
+                    <a-sky ${className} src="#${props.id}" ${
+                    color.attribute
+                  } animation="property: rotation; to: ${
+                    rotation.attributes
+                  }; dur: 10" preload="auto"
                     animation__fade="property: components.material.material.color; type: color; from: #FFF; to: #000; dur: 300; startEvents: fade"
                     animation__fadeback="property: components.material.material.color; type: color; from: #000; to: #FFF; dur: 300; startEvents: animationcomplete__fade">
                     </a-sky>`;
@@ -157,7 +186,9 @@ export class StoryboxAframe {
                 if (props.art !== undefined) {
                   // https://aframe.io/docs/0.9.0/components/gltf-model.html
                   assetsElements.push(
-                    `<a-asset-item ${className} id="${props.id}" src="${props.art}" preload="auto"></a-asset-item>`
+                    `<a-asset-item ${className} id="${props.id}" src="${
+                      props.art
+                    }" preload="auto"></a-asset-item>`
                   );
 
                   innerMarkup = `${innerMarkup}
@@ -174,21 +205,29 @@ export class StoryboxAframe {
                 }
                 break;
               case "camera":
-              let touchContollers = ``;
-              if (props.touch !== undefined) {
-                // https://aframe.io/docs/0.9.0/introduction/interactions-and-controllers.html
-                // Can change hand controller
-                let leftModel = ``;
-                let rightModel = ``;
-                if (props.touch.left.glb !== undefined) {
-                  let leftModelScale = this.getAxis("scale", props.touch.left);
-                  let leftModelPosition = this.getAxis("position", props.touch.left);
-                  // https://aframe.io/docs/0.9.0/components/gltf-model.html
-                  assetsElements.push(
-                    `<a-asset-item id="${props.touch.left.id}" src="${props.touch.left.glb}" preload="auto"></a-asset-item>`
-                  );
+                let touchContollers = ``;
+                if (props.touch !== undefined) {
+                  // https://aframe.io/docs/0.9.0/introduction/interactions-and-controllers.html
+                  // Can change hand controller
+                  let leftModel = ``;
+                  let rightModel = ``;
+                  if (props.touch.left.glb !== undefined) {
+                    let leftModelScale = this.getAxis(
+                      "scale",
+                      props.touch.left
+                    );
+                    let leftModelPosition = this.getAxis(
+                      "position",
+                      props.touch.left
+                    );
+                    // https://aframe.io/docs/0.9.0/components/gltf-model.html
+                    assetsElements.push(
+                      `<a-asset-item id="${props.touch.left.id}" src="${
+                        props.touch.left.glb
+                      }" preload="auto"></a-asset-item>`
+                    );
 
-                  leftModel  = `
+                    leftModel = `
                     <a-entity
                     ${className}
                     gltf-model="#${props.touch.left.id}"
@@ -198,18 +237,28 @@ export class StoryboxAframe {
                     preload="true"
                     >
                     </a-entity>`;
-                }
+                  }
 
+                  if (
+                    props.touch.right !== undefined &&
+                    props.touch.right.glb !== undefined
+                  ) {
+                    let rightModelScale = this.getAxis(
+                      "scale",
+                      props.touch.right
+                    );
+                    let rightModelPosition = this.getAxis(
+                      "position",
+                      props.touch.right
+                    );
+                    // https://aframe.io/docs/0.9.0/components/gltf-model.html
+                    assetsElements.push(
+                      `<a-asset-item id="${props.touch.right.id}" src="${
+                        props.touch.right.glb
+                      }" preload="auto"></a-asset-item>`
+                    );
 
-                if (props.touch.right !== undefined && props.touch.right.glb !== undefined) {
-                  let rightModelScale = this.getAxis("scale", props.touch.right);
-                  let rightModelPosition = this.getAxis("position", props.touch.right);
-                  // https://aframe.io/docs/0.9.0/components/gltf-model.html
-                  assetsElements.push(
-                    `<a-asset-item id="${props.touch.right.id}" src="${props.touch.right.glb}" preload="auto"></a-asset-item>`
-                  );
-
-                  rightModel  = `
+                    rightModel = `
                     <a-entity
                     ${className}
                     gltf-model="#${props.touch.right.id}"
@@ -219,22 +268,25 @@ export class StoryboxAframe {
                     preload="true"
                     >
                     </a-entity>`;
-                }
+                  }
 
+                  touchContollers = `
+                <a-entity id="leftHand" oculus-touch-controls="hand:left" x-button-listener y-button-listener>${leftModel}</a-entity>
+                <a-entity id="rightHand" oculus-touch-controls="hand:right" a-button-listener b-button-listener>${rightModel}</a-entity>`;
+              } else {
                 touchContollers = `
-                <a-entity id="leftHand" oculus-touch-controls="hand:left" x-button-listener>${leftModel}</a-entity>
-                <a-entity id="rightHand" oculus-touch-controls="hand:right" x-button-listener>${rightModel}</a-entity>`;
+                <a-entity id="leftHand" oculus-touch-controls="hand:left" x-button-listener y-button-listener></a-entity>
+                <a-entity id="rightHand" oculus-touch-controls="hand:right" a-button-listener b-button-listener></a-entity>`;
               }
-              if (props.laser !== undefined) {
-                // https://aframe.io/docs/0.9.0/introduction/interactions-and-controllers.html
-                // Can change hand controller
-                let leftLine = this.getProperties('line', props.left);
-                let rightLine = this.getProperties('line', props.right);
-                innerMarkup = `${innerMarkup}
+                if (props.laser !== undefined) {
+                  // https://aframe.io/docs/0.9.0/introduction/interactions-and-controllers.html
+                  // Can change hand controller
+                  let leftLine = this.getProperties("line", props.left);
+                  let rightLine = this.getProperties("line", props.right);
+                  innerMarkup = `${innerMarkup}
                 <a-entity laser-controls="hand: left" ${leftLine}></a-entity>
                 <a-entity laser-controls="hand: right" ${rightLine}></a-entity>`;
-              }
-
+                }
 
                 let fadeMask = ``;
                 if (props.fadeMask === true) {
@@ -243,7 +295,7 @@ export class StoryboxAframe {
                   //     <a-animation attribute="material.opacity" begin="fade" to="1"></a-animation>
                   //   </a-entity>`;
 
-                    fadeMask = `
+                  fadeMask = `
                       <a-box scale="200 200 200" position="0 0 -10" color="#000000">
                       </a-box>`;
                 }
@@ -252,7 +304,9 @@ export class StoryboxAframe {
                   // console.log("Setting up Cursor Camera", props);
                   innerMarkup = `${innerMarkup}
                     <a-entity ${className} id="${props.id}" ${position.tag}>
-                      <a-camera ${className} id="${props.id}" look-controls wasd-controls>
+                      <a-camera ${className} id="${
+                    props.id
+                  }" look-controls wasd-controls>
                       <a-entity
                         cursor="fuse: true"
                         material="color: black; shader: flat"
@@ -279,35 +333,55 @@ export class StoryboxAframe {
                 break;
               case "light":
                 innerMarkup = `${innerMarkup}
-                  <a-light ${className} type="${props.type ? props.type : 'point'}" ${color.tag} ${position.tag}>
+                  <a-light ${className} type="${
+                  props.type ? props.type : "point"
+                }" ${color.tag} ${position.tag}>
                   </a-light>`;
                 break;
-              case 'audio':
-                assetsElements.push(`<audio id="${props.id}" ${className} preload="auto" crossorigin="anonymous" src="${props.src}" ${position.tag}></audio>`);
-                break;
-              case 'image':
+              case "audio":
                 assetsElements.push(
-                  `<img ${className} id="${props.id}" src="${props.art}" crossorigin="anonymous" preload="true" />`
+                  `<audio id="${
+                    props.id
+                  }" ${className} preload="auto" crossorigin="anonymous" src="${
+                    props.src
+                  }" ${position.tag}></audio>`
+                );
+                break;
+              case "image":
+                assetsElements.push(
+                  `<img ${className} id="${props.id}" src="${
+                    props.art
+                  }" crossorigin="anonymous" preload="true" />`
                 );
                 // HACK force a quick rotation to easily fix any weirdly oriented skybox art.
                 innerMarkup = `${innerMarkup}
-                  <a-image ${className} src="#${props.id}" material="alphaTest: 0.5" ${position.tag} ${scale.tag} ${rotation.tag}>
+                  <a-image ${className} src="#${
+                  props.id
+                }" material="alphaTest: 0.5" ${position.tag} ${scale.tag} ${
+                  rotation.tag
+                }>
                   </a-image>`;
-                  break;
-              case 'imagecube':
+                break;
+              case "imagecube":
                 let cube = this.getCube(props);
                 cube.map((face, index) => {
                   assetsElements.push(
-                    `<img ${className} id="${props.id}-${index}" src="${face.art}" crossorigin="anonymous" preload="true" />`
+                    `<img ${className} id="${props.id}-${index}" src="${
+                      face.art
+                    }" crossorigin="anonymous" preload="true" />`
                   );
 
                   innerMarkup = `${innerMarkup}
-                    <a-image ${className} src="#${props.id}-${index}" ${face.position} ${scale.tag} ${face.rotation}></a-image>`;
+                    <a-image ${className} src="#${props.id}-${index}" ${
+                    face.position
+                  } ${scale.tag} ${face.rotation}></a-image>`;
                 });
                 break;
-              case 'text':
+              case "text":
                 innerMarkup = `${innerMarkup}
-                  <a-entity ${text.tag} ${position.tag} ${scale.tag}></a-entity>`;
+                  <a-entity ${text.tag} ${position.tag} ${
+                  scale.tag
+                }></a-entity>`;
                 break;
               case "box":
                 if (props.type !== undefined && props.type === "button") {
@@ -347,7 +421,7 @@ export class StoryboxAframe {
 
       return {
         assetsElements: assetsElements,
-        innerMarkup: innerMarkup,
+        innerMarkup: innerMarkup
       };
     }
   }
