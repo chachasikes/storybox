00ab1b4ae707561c7e65184d82b9a9f11bdddcd7
Chacha Sikes <chachasikes@gmail.com>
2019-06-02 10:05:42 -0700

    rename _scenes

--- public/examples/registry.js
+++ public/examples/registry.js
@@ -1,5 +1,5 @@
-import { story as storyHelloWorld } from '/examples/scenes/hello-world/_scenes.js';
-import { story as seaweed } from '/examples/scenes/scent-demo-seaweed/_scenes.js';
+import { story as storyHelloWorld } from '/examples/scenes/hello-world/scenes.js';
+import { story as seaweed } from '/examples/scenes/scent-demo-seaweed/scenes.js';
 export let registry = [
   storyHelloWorld,
   seaweed
