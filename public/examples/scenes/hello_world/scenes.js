92ed4e6852db1ef83730baa0ae0718b6a2ce75fc
Chacha Sikes <chachasikes@gmail.com>
2019-06-02 10:10:09 -0700

    fix path

--- public/examples/scenes/hello_world/scenes.js
+++ public/examples/scenes/hello_world/scenes.js
@@ -5,7 +5,7 @@ import { Scene as s2 } from './scene-02.js';
 export let story = {
   id: 'hello_world',
   name: 'Hello World',
-  panel: '/examples/scenes/hello_world/images/puppy.png',
+  panel: './examples/scenes/hello_world/images/puppy.png',
   scenes: [
     start,
     s1,
