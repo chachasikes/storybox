180e6828b974405e87f44fc0f9ff72501f59b86a
Chacha Sikes <chachasikes@gmail.com>
2019-06-03 06:31:25 -0700

    set up rose accordion

--- public/examples/gallery/registry.js
+++ public/examples/gallery/registry.js
@@ -1,6 +1,8 @@
 import { story as storyHelloWorld } from './../scenes/hello_world/scenes.js';
+import { story as roseAccordion } from './../scenes/rose_accordion/scenes.js';
 // import { dropboxRegistry } from './../../dropbox/dropbox-tincture-sea.js';
 
 export let registry = [
-  storyHelloWorld
+  storyHelloWorld,
+  roseAccordion
 ];
---
+++ public/examples/scenes/rose_accordion/scenes.js
@@ -0,0 +1,10 @@
+import { Scene as start } from './start.js';
+
+export let story = {
+  id: 'rose_accordion',
+  name: 'Rose Accordion',
+  panel: './examples/scenes/rose_accordion/images/rose.png',
+  scenes: [
+    start,
+  ]
+};
