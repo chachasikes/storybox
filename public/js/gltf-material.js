0605976e48f1f4853d95e4f0687c4c08df7d9ed2
Chacha Sikes <chachasikes@gmail.com>
2019-08-13 15:41:10 -0700

    improve some texture loaders

--- public/js/gltf-material.js
+++ public/js/gltf-material.js
@@ -1,9 +1,16 @@
 AFRAME.registerComponent('gltf-material', {
     schema: {
       path: {default: ''},
-      bumpPath: {default: ''},
-      roughnessPath: {default: ''},
-      ambientOcculsionPath: {default: ''},
+      normalPath: {default: null},
+      bumpPath: {default: null},
+      alphaPath: {default: null},
+      displacementPath: {default: null},
+      roughnessPath: {default: null},
+      environmentPath: {default: null},
+      emissivePath: {default: null},
+      lightMapPath: {default: null},
+      ambientOcculsionPath: {default: null},
+      opacity: {default: 1.0},
       extension: {default: 'jpg'},
       format: {default: 'RGBFormat'},
       enableBackground: {default: false}
@@ -11,7 +18,7 @@ AFRAME.registerComponent('gltf-material', {
     init: function() {
       const data = this.data;
       var el = this.el;
-      console.log(data);
+
       let materialSettings = {};
       // https://threejs.org/docs/#api/en/materials/MeshStandardMaterial
 
@@ -19,74 +26,22 @@ AFRAME.registerComponent('gltf-material', {
         u: 8,
         v: 8
       }
-      if (data.normalPath !== undefined) {
-        var normalPathTexture = new THREE.TextureLoader().load(data.normalPath);
-        normalPathTexture.wrapS = THREE.RepeatWrapping;
-        normalPathTexture.wrapT = THREE.RepeatWrapping;
-        normalPathTexture.repeat.set(scale.u, scale.v);
-        materialSettings.map = normalPathTexture;
-      }
 
-      if (data.bumpPath !== undefined) {
-        var bumpTexture = new THREE.TextureLoader().load(data.bumpPath);
-        bumpTexture.wrapS = THREE.RepeatWrapping;
-        bumpTexture.wrapT = THREE.RepeatWrapping;
-        bumpTexture.repeat.set(scale.u, scale.v);
-        materialSettings.bumpMap = bumpTexture;
-        materialSettings.bumpScale = 0.5;
-      }
+      materialSettings.metalness = 0;
+      // materialSettings.side = "double";
+      // materialSettings.alphaTest = 0;
+      // materialSettings.flatShading = true;
 
-      if (data.alphaPath !== undefined) {
-        var alphaTexture = new THREE.TextureLoader().load(data.alphaPath);
-        alphaTexture.wrapS = THREE.RepeatWrapping;
-        alphaTexture.wrapT = THREE.RepeatWrapping;
-        alphaTexture.repeat.set(scale.u, scale.v);
-        materialSettings.alphaMap = alphaTexture;
-      }
+      // materialSettings.transparent = true;
+      //     color : 0x000000,
+      //     side: "double",
+      //     alphaTest: 100,
+      //     flatShading: true,
+      //     transparent: true,
+      //     opacity: 0.5,
 
-      if (data.displacementPath !== undefined) {
-        var displacementTexture = new THREE.TextureLoader().load(data.displacementPath);
-        displacementTexture.wrapS = THREE.RepeatWrapping;
-        displacementTexture.wrapT = THREE.RepeatWrapping;
-        displacementTexture.repeat.set(scale.u, scale.v);
-        materialSettings.displacementMap = displacementTexture;
-        // materialSettings.displacementScale = 0.5;
-      }
-
-      if (data.environmentPath !== undefined) {
-        var environmentTexture = new THREE.TextureLoader().load(data.environmentPath);
-        environmentTexture.wrapS = THREE.RepeatWrapping;
-        environmentTexture.wrapT = THREE.RepeatWrapping;
-        environmentTexture.repeat.set(scale.u, scale.v);
-        materialSettings.environmentMap = environmentTexture;
-        // materialSettings.envMapIntensity = 2;
-      }
-
-      if (data.emissivePath !== undefined) {
-        var emissiveTexture = new THREE.TextureLoader().load(data.emissivePath);
-        emissiveTexture.wrapS = THREE.RepeatWrapping;
-        emissiveTexture.wrapT = THREE.RepeatWrapping;
-        emissiveTexture.repeat.set(scale.u, scale.v);
-        materialSettings.emissiveMap = emissiveTexture;
-      }
-
-      if (data.ambientOcculsionPath !== undefined) {
-        var ambientOcculsionTexture = new THREE.TextureLoader().load(data.ambientOcculsionPath);
-        materialSettings.aoMap = ambientOcculsionTexture;
-        materialSettings.aoMapIntensity = 0.1;
-      }
-
-      if (data.roughnessPath !== undefined) {
-        var roughnessTexture = new THREE.TextureLoader().load(data.roughnessPath);
-        roughnessTexture.wrapS = THREE.RepeatWrapping;
-        roughnessTexture.wrapT = THREE.RepeatWrapping;
-        roughnessTexture.repeat.set(scale.u, scale.v);
-        materialSettings.roughnessMap = roughnessTexture;
-      }
-
-      if (data.lightMapPath !== undefined) {
-        var lightMapTexture = new THREE.TextureLoader().load(data.lightMapPath);
-        materialSettings.lightMap = lightMapTexture;
+      if (data.opacity !== undefined) {
+        materialSettings.opacity = 0.5;
       }
 
       if (data.path !== undefined) {
@@ -97,11 +52,80 @@ AFRAME.registerComponent('gltf-material', {
         materialSettings.map = baseTexture;
       }
 
-      if (data.opacity !== undefined) {
-        materialSettings.opacity = data.opacity;
+      if (data.normalPath !== undefined) {
+        var normalPathTexture = new THREE.TextureLoader().load(data.normalPath);
+        normalPathTexture.wrapS = THREE.RepeatWrapping;
+        normalPathTexture.wrapT = THREE.RepeatWrapping;
+        normalPathTexture.repeat.set(scale.u, scale.v);
+        materialSettings.normalMap = normalPathTexture;
       }
 
-      materialSettings.metalness = 0;
+      if (data.bumpPath !== null) {
+        var bumpTexture = new THREE.TextureLoader().load(data.bumpPath);
+        bumpTexture.wrapS = THREE.RepeatWrapping;
+        bumpTexture.wrapT = THREE.RepeatWrapping;
+        bumpTexture.repeat.set(scale.u, scale.v);
+        materialSettings.bumpMap = bumpTexture;
+        materialSettings.bumpScale = 0.5;
+      }
+
+      if (data.alphaPath !== null) {
+        var alphaTexture = new THREE.TextureLoader().load(data.alphaPath);
+        alphaTexture.wrapS = THREE.RepeatWrapping;
+        alphaTexture.wrapT = THREE.RepeatWrapping;
+        alphaTexture.repeat.set(scale.u, scale.v);
+        materialSettings.alphaMap = alphaTexture;
+      }
+
+      if (data.displacementPath !== null) {
+        var displacementTexture = new THREE.TextureLoader().load(data.displacementPath);
+        displacementTexture.wrapS = THREE.RepeatWrapping;
+        displacementTexture.wrapT = THREE.RepeatWrapping;
+        displacementTexture.repeat.set(scale.u, scale.v);
+        materialSettings.displacementMap = displacementTexture;
+        // materialSettings.displacementScale = 0.5;
+      }
+
+      if (data.environmentPath !== null) {
+        var environmentTexture = new THREE.TextureLoader().load(data.environmentPath);
+        environmentTexture.wrapS = THREE.RepeatWrapping;
+        environmentTexture.wrapT = THREE.RepeatWrapping;
+        environmentTexture.repeat.set(scale.u, scale.v);
+        materialSettings.envMap = environmentTexture;
+        // materialSettings.envMapIntensity = 2;
+      }
+
+      if (data.emissivePath !== null) {
+        var emissiveTexture = new THREE.TextureLoader().load(data.emissivePath);
+        emissiveTexture.wrapS = THREE.RepeatWrapping;
+        emissiveTexture.wrapT = THREE.RepeatWrapping;
+        emissiveTexture.repeat.set(scale.u, scale.v);
+        materialSettings.emissiveMap = emissiveTexture;
+      }
+
+      if (data.ambientOcculsionPath !== null) {
+        var ambientOcculsionTexture = new THREE.TextureLoader().load(data.ambientOcculsionPath);
+        materialSettings.aoMap = ambientOcculsionTexture;
+        materialSettings.aoMapIntensity = 0.1;
+      }
+      //
+      if (data.roughnessPath !== null) {
+        var roughnessTexture = new THREE.TextureLoader().load(data.roughnessPath);
+        roughnessTexture.wrapS = THREE.RepeatWrapping;
+        roughnessTexture.wrapT = THREE.RepeatWrapping;
+        roughnessTexture.repeat.set(scale.u, scale.v);
+        materialSettings.roughnessMap = roughnessTexture;
+      }
+
+      if (data.lightMapPath !== null) {
+        var lightMapTexture = new THREE.TextureLoader().load(data.lightMapPath);
+        materialSettings.lightMap = lightMapTexture;
+      }
+
+
+
+
+
       // materialSettings.emissive = #ffffff;
       // materialSettings.emissiveIntensity = 0.3;
       this.material = new THREE.MeshStandardMaterial(materialSettings);
