45115bc14cff8b278305ee8c635a24f97835203b
Chacha Sikes <chachasikes@gmail.com>
2019-11-29 22:42:46 -0800

    rename rose file placeholders to new version names

--- public/examples/scenes/rose_accordion/rose_string_1.js
+++ public/examples/scenes/rose_accordion/rose_string_1.js
@@ -5,6 +5,13 @@ export let Scene = {
   name: "Rose Accordion",
   duration: 60000,
   autoPlay: false,
+  skybox: {
+    sky: {
+      color: "#ededed",
+      id: "light-background",
+      name: "Fade in"
+    }
+  },
   camera_1: {
     camera: {
       name: "World camera",
@@ -76,44 +83,44 @@ export let Scene = {
               y: 0.001,
               z: 0
             },
-            color: "#f4eea8",
-            id: "citronellol-intersect",
+            color: "#888888",
+            id: "rose_metal_intersect",
             intersect: true,
             collisionClass: "head",
-            intersectTarget: "nosina_1",
+            intersectTarget: "rose_metal",
             intersectAction: 'fadeInObject',
             radius: 0.2,
           },
-          // {
-          //   type: "percentage",
-          //   position: {
-          //     x: 0.6,
-          //     y: 0.001,
-          //     z: 0
-          //   },
-          //   color: "#f4a8e1",
-          //   id: "pea-intersect",
-          //   intersect: true,
-          //   collisionClass: "head",
-          //   intersectTarget: "nosina_2",
-          //   intersectAction: 'fadeInObject',
-          //   radius: 0.2,
-          // },
-          // {
-          //   type: "percentage",
-          //   position: {
-          //     x: 0.85,
-          //     y: 0.001, // These would just be offsets
-          //     z: 0
-          //   },
-          //   color: "#bbe266",
-          //   id: "geraniol-intersect",
-          //   intersect: true,
-          //   collisionClass: "head",
-          //   intersectTarget: "nosina_3",
-          //   intersectAction: 'fadeInObject',
-          //   radius: 0.2,
-          // }
+          {
+            type: "percentage",
+            position: {
+              x: 0.6,
+              y: 0.001,
+              z: 0
+            },
+            color: "#f4a8e1",
+            id: "rosy_peachy_intersect",
+            intersect: true,
+            collisionClass: "head",
+            intersectTarget: "rosy_peachy",
+            intersectAction: 'fadeInObject',
+            radius: 0.2,
+          },
+          {
+            type: "percentage",
+            position: {
+              x: 0.85,
+              y: 0.001, // These would just be offsets
+              z: 0
+            },
+            color: "#249411",
+            id: "four_am_intersect",
+            intersect: true,
+            collisionClass: "head",
+            intersectTarget: "four_am",
+            intersectAction: 'fadeInObject',
+            radius: 0.2,
+          }
         ]
       }
     }
@@ -129,48 +136,17 @@ export let Scene = {
       color: "#ffffff"
     }
   },
-  skybox: {
-    sky: {
-      color: "#ededed",
-      id: "light-background",
-      name: "Fade in"
-    }
-  },
-  grid: {
-    imagecube: {
-      art: "./images/grid-01.png",
-      id: "grid-bkg",
-      name: "Grid",
-      credit: "Chacha Sikes",
-      rotation: {
-        x: 0,
-        y: 0,
-        z: 0
-      },
-      position: {
-        x: 0,
-        y: 5,
-        z: 0
-      },
-      scale: {
-        x: 10,
-        y: 10,
-        z: 10
-      }
-    }
-  },
-  nosina: {
+  rose_metal: {
     mesh: {
-      name: "Nosina",
+      name: "Rose Metal",
       credit: "Sarah Chalek & Chacha Sikes",
-      id: "nosina_1",
-      art:
-        "https://www.dropbox.com/s/z9q199hf0utq5px/nosina_alembic_test.glb?dl=0",
-      opacity: 0.1,
+      id: "rose_metal",
+      art: "https://www.dropbox.com/s/dsknrain4eopjt7/rose_metal.glb?dl=0",
+      opacity: 0,
       scale: {
-        x: 1.5,
-        y: 1.5,
-        z: 1.5
+        x: 1,
+        y: 1,
+        z: 1
       },
       rotation: {
         x: 0,
@@ -179,15 +155,15 @@ export let Scene = {
       },
       position: {
         x: 0,
-        y: 3,
-        z: -4
+        y: 0,
+        z: -10
       },
       events: [{
-        id: 'nosina_1',
+        id: 'rose_metal',
         eventName: 'fadeInObject',
         type: 'intersection-play',
         attribute: "gltf-model-opacity",
-        duration: "10000",
+        duration: "5000",
         from: "0",
         to: "1",
         repeat: true,
@@ -199,93 +175,106 @@ export let Scene = {
       }]
     }
   },
-  // nosina2: {
-  //   mesh: {
-  //     name: "Nosina",
-  //     credit: "Sarah Chalek & Chacha Sikes",
-  //     id: "nosina_2",
-  //     art:
-  //       "https://www.dropbox.com/s/z9q199hf0utq5px/nosina_alembic_test.glb?dl=0",
-  //     opacity: 0.5,
-  //     scale: {
-  //       x: 5,
-  //       y: 5,
-  //       z: 5
-  //     },
-  //     rotation: {
-  //       x: 0,
-  //       y: 210,
-  //       z: 0
-  //     },
-  //     position: {
-  //       x: -2,
-  //       y: 3,
-  //       z: -4
-  //     },
-  //     fadeInObject: {
-  //       attribute: "model-opacity",
-  //       duraction: "10000",
-  //       from: "0",
-  //       to: "1",
-  //       repeat: "indefinite"
-  //     }
-  //   }
-  // },
-  // nosina3: {
-  //   mesh: {
-  //     name: "Nosina",
-  //     credit: "Sarah Chalek & Chacha Sikes",
-  //     id: "nosina_3",
-  //     art:
-  //       "https://www.dropbox.com/s/z9q199hf0utq5px/nosina_alembic_test.glb?dl=0",
-  //     opacity: 1,
-  //     scale: {
-  //       x: 1,
-  //       y: 1,
-  //       z: 1
-  //     },
-  //     rotation: {
-  //       x: 0,
-  //       y: 210,
-  //       z: 0
-  //     },
-  //     position: {
-  //       x: 2,
-  //       y: 3,
-  //       z: -4
-  //     },
-  //     fadeInObject: {
-  //       attribute: "model-opacity",
-  //       duraction: "10000",
-  //       from: "0",
-  //       to: "1",
-  //       repeat: "indefinite"
-  //     }
-  //   }
-  // }
-
-  // train_station: {
-  //   mesh: {
-  //     name: "Set",
-  //     credit: "Chacha Sikes & Sarah Chalek",
-  //     id: "train-station",
-  //     art: "https://www.dropbox.com/s/bca80aqnz14chxb/rose-accordion-animation.glb?dl=0",
-  //     opacity: 1,
-  //     scale: {
-  //       x: 10,
-  //       y: 10,
-  //       z: 10,
-  //     },
-  //     rotation: {
-  //       x: 0,
-  //       y: 0,
-  //       z: 0
-  //     },
-  //     position: {
-  //       x: 4,
-  //       y: 0,
-  //       z: 2.5
-  //     }
-  //   },
-  // },
+  nosina2: {
+    mesh: {
+      name: "Rosy Peachy",
+      credit: "Sarah Chalek & Chacha Sikes",
+      id: "rosy_peachy",
+      art: "https://www.dropbox.com/s/dsknrain4eopjt7/rose_metal.glb?dl=0",
+      opacity: 0,
+      scale: {
+        x: 1,
+        y: 1,
+        z: 1
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      position: {
+        x: -5,
+        y: 0,
+        z: -10
+      },
+      events: [{
+        id: 'rosy_peachy',
+        eventName: 'fadeInObject',
+        type: 'intersection-play',
+        attribute: "gltf-model-opacity",
+        duration: "5000",
+        from: "0",
+        to: "1",
+        repeat: true,
+        elasticity: 400,
+        loop: false,
+        delay: 0,
+        dir: 'alternate',
+        round: false
+      }]
+    }
+  },
+  four_am: {
+    mesh: {
+      name: "four_am",
+      credit: "Sarah Chalek & Chacha Sikes",
+      id: "four_am",
+      art: "https://www.dropbox.com/s/dsknrain4eopjt7/rose_metal.glb?dl=0",
+      opacity: 0,
+      scale: {
+        x: 1,
+        y: 1,
+        z: 1
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      position: {
+        x: 5,
+        y: 0,
+        z: -10
+      },
+      events: [{
+        id: 'four_am',
+        eventName: 'fadeInObject',
+        type: 'intersection-play',
+        attribute: "gltf-model-opacity",
+        duration: "5000",
+        from: "0",
+        to: "1",
+        repeat: true,
+        elasticity: 400,
+        loop: false,
+        delay: 0,
+        dir: 'alternate',
+        round: false
+      }]
+    }
+  },
+  train_station: {
+    mesh: {
+      name: "Set",
+      credit: "Chacha Sikes & Sarah Chalek",
+      id: "train-station",
+      art: "https://www.dropbox.com/s/u3i0e93bi7uu82y/train_station.glb?dl=0",
+      opacity: 1,
+      scale: {
+        x: 100,
+        y: 100,
+        z: 100,
+      },
+      rotation: {
+        x: 0,
+        y: 0,
+        z: 0
+      },
+      position: {
+        x: 0,
+        y: -20,
+        z: 0
+      }
+    },
+  }
 };
